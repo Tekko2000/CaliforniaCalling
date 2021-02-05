895273c52888b7a447fe875f77013e9149bc184f
Tekko2000 <ot1977@gmx.net>
2021-02-05 08:54:40 +0100

    California

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+function lightMode() {
+    document.getElementById('header').style.color = "#000";
+    document.getElementById('header').style.background = "#fff";
+    document.getElementById('sec1').style.color = "#000";
+    document.getElementById('sec1').style.background = "#fff";
+    document.getElementById('sec2').style.color = "#fff";
+    document.getElementById('sec2').style.background = "#000";
+    document.getElementById('art1').style.color = "#000";
+    document.getElementById('art1').style.background = "#fff";
+    document.getElementById('sec3').style.color = "#fff";
+    document.getElementById('sec3').style.background = "#000";
+    document.getElementById('footer').style.color = "#000";
+    document.getElementById('footer').style.background = "#fff";
+}
+function darkMode() {
+    document.getElementById('header').style.color = "#fff";
+    document.getElementById('header').style.background = "#000";
+    document.getElementById('sec1').style.color = "#fff";
+    document.getElementById('sec1').style.background = "#000";
+    document.getElementById('sec2').style.color = "#000";
+    document.getElementById('sec2').style.background = "#fff";
+    document.getElementById('art1').style.color = "#fff";
+    document.getElementById('art1').style.background = "#000";
+    document.getElementById('sec3').style.color = "#000";
+    document.getElementById('sec3').style.background = "#fff";
+    document.getElementById('footer').style.color = "#fff";
+    document.getElementById('footer').style.background = "#000";
+}
